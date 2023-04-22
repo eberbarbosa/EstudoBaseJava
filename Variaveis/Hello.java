3c258b97ac5f6adda118c48bb0c125d4227fb258
eberbarbosa <eberbarbosa255@gmail.com>
2023-04-22 17:04:45 -0300

    teste 1 pasta variavel

---
+++ Variaveis/Hello.java
@@ -0,0 +1,14 @@
+
+
+/**
+ *
+ * @author eber
+ */
+public class Hello {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("!! Hello World !!");
+    }
+    
+}
