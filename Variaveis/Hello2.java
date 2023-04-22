4bc323f2b65cc036ee811b544206e9293fb6c7b6
eberbarbosa <eberbarbosa255@gmail.com>
2023-04-22 18:22:50 -0300

    Iniciando estudos de variáveis

---
+++ Variaveis/Hello2.java
@@ -0,0 +1,20 @@
+/*
+ * Essa é outra variação do classico Hello World. Essa versão cria um objeto ola,
+ * esse objeto é passado para o método println()
+ */
+
+/**
+ *
+ * @author eber
+ */
+public class Hello2 {
+    
+    public static void main(String[] args) {
+        
+        String ola = "!! Hello World !!";
+        
+        System.out.println(ola);        
+        
+    }
+    
+}
