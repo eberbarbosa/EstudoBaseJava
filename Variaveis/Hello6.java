a8d1b59d9f7337415b499ee22c94934ef42512f0
eberbarbosa <eberbarbosa255@gmail.com>
2023-04-23 10:25:28 -0300

    Exercicios com variaveis

---
+++ Variaveis/Hello6.java
@@ -0,0 +1,18 @@
+/*
+ * 23/04/2023
+ * Imprime multiplas linha de texto com uma única instrução - > \n
+ */
+
+/**
+ *
+ * @author eber
+ */
+public class Hello6 {  
+        
+    public static void main(String[] args) {
+        
+        System.out.println("Olá\nPessoal,\nVamos\nEstudar\n!! Java !!\n");               
+        
+    }
+    
+}
