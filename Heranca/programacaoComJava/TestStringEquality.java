8b78a48c6340af533e5753e5e283ed9a133b5d11
eberbarbosa <eberbarbosa255@gmail.com>
2024-01-15 15:24:04 -0300

    Término estudo Herança

---
+++ Heranca/programacaoComJava/TestStringEquality.java
@@ -0,0 +1,25 @@
+/*
+ *  28/10/2023 
+ * - pagina 295 livro: Programação com Java - John Hubbard
+ *  Método equals() da classe String
+ *  
+ */
+package programacaoComJava;
+
+/**
+ *
+ * @author eber
+ */
+public class TestStringEquality {
+    
+    public static void main(String[] args) {
+        
+        String s2 = "ABC";
+        String s3 = new String ("ABC");
+        
+        System.out.println("s3 == s3: " + (s3 == s2));
+        
+        System.out.println("s3.equals(s2): " + s3.equals(s2));
+    }
+    
+}
