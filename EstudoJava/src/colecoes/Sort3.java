b561d98b781e2cae0bf451028ad38c917f53785d
Eber Barbosa <eberbarbosa255@gmail.com>
2024-09-13 15:47:52 -0300

    Commit Atualização estudo coleções

---
+++ EstudoJava/src/colecoes/Sort3.java
@@ -0,0 +1,44 @@
+/*
+ *  Data: 12/09/2024
+ * - pagina 690 livro: Java como Programar - Deitel
+ *  Descrição: Classifica uma lista utilizando a classe Comparator TimeComparator personalizada
+ */
+
+
+package colecoes;
+
+import javaComoProgramar.Time2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort3 {
+
+    public void printElements() {
+
+        List<Time2> list = new ArrayList<Time2>();      // cria a lista
+
+        list.add(new Time2( 6, 24, 34 ) );
+        list.add(new Time2( 18, 14, 58 ) );
+        list.add(new Time2( 6, 05, 34 ) );
+        list.add(new Time2( 12, 14, 58 ) );
+        list.add(new Time2( 6, 24, 22 ) );
+
+        // gera a saída de elementos List
+        System.out.printf("Elementos da matriz não classificados:\n%s\n", list);
+
+        // classifica em ordem utilizando um comparador
+        Collections.sort(list, new TimeComparator());
+
+        // gera a saída de elementos List
+        System.out.printf("Elementos da matriz classificados:\n%s\n", list);
+    }
+
+    public static void main(String[] args) {
+
+        Sort3 sort3 = new Sort3();
+        sort3.printElements();
+    }
+}
---
+++ EstudoJava/src/colecoes/TimeComparator.java
@@ -0,0 +1,33 @@
+/*
+ *  Data: 12/09/2024
+ * - pagina 689 livro: Java como Programar - Deitel
+ *  Descrição: Classe Comparator personalizada que compara dois objetos Time2
+ */
+
+package colecoes;
+
+import javaComoProgramar.Time2;
+
+import java.util.Comparator;
+
+public class TimeComparator implements Comparator<Time2> {
+
+    public int compare(Time2 time1, Time2 time2) {
+
+        int hourCompare   = time1.getHour() - time2.getHour();      // compara a hora
+        int minuteCompare = time2.getMinute() - time2.getMinute();  // compara o minuto
+        int secondCompare = time2.getSecond() - time2.getSecond();  // compata o segundo
+
+        // testa a primeira hora
+        if(hourCompare != 0) {
+            return hourCompare;
+        }
+
+        // então testa o minuto
+        if(minuteCompare != 0) {
+            return minuteCompare;
+        }
+
+        return secondCompare;       // retorna a comparação de segundos
+    }
+}
